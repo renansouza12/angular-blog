3526ae1e68f610460240c573dc151d22253d0a2d
RenanSouza <renansouza0122@gmail.com>
2023-09-15 16:18:56 -0300

    dynamic componentes created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component ,Input} from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.scss']
+  styleUrls: ['./big-card.component.scss',
+  './big-card.responsive.component.scss']
 })
 export class BigCardComponent {
 
+  @Input()
+  photoCover!:string;
+  @Input()
+  cardTitle:string = "";
+  @Input()
+  cardDescription = "";
 }
