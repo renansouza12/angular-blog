06341aa9ead40cde7d781acd4dffba13e6ec4d45
RenanSouza <renansouza0122@gmail.com>
2023-09-20 20:07:32 -0300

    card updated

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
     {
         "id":"1",
-        "title":"title",
-        "description":"Description",
-        "photoCover": "https://www.profesionalreview.com/wp-content/uploads/2023/09/iPhone-15-Pro-y-15-Pro-Max.jpg"
+        "title":"Scenic Mountain Landscape",
+        "description":"Experience the breathtaking beauty of nature with our scenic mountain landscape. Towering peaks, lush green valleys, and crystal-clear lakes await you in this stunning image. Whether you're an avid hiker or simply a lover of the great outdoors, this picture captures the essence of tranquility and adventure.",
+        "photoCover": "https://images.unsplash.com/photo-1463693396721-8ca0cfa2b3b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
     },
     {
         "id":"2",
-        "title":"title",
-        "description":"Description",
-        "photoCover": "https://i.pinimg.com/564x/4f/df/c7/4fdfc7881713bc49a89a3f2828f62718.jpg"
-    }
+        "title":"Sunset Over the Ocean",
+        "description":"Witness the magic of a coastal sunset as the sun dips below the horizon, casting a warm and golden glow over the tranquil waters. The serene beauty of this scene is perfect for those seeking a moment of relaxation and reflection.",
+        "photoCover": "https://images.unsplash.com/photo-1655989636880-c5ec0f94f4eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1776&q=80"
+    },
+    {
+        "id":"3",
+        "title":"Cityscape at Night",
+        "description":"Discover the vibrant energy of the city that never sleeps with our captivating cityscape at night. The neon lights, bustling streets, and towering skyscrapers create a dynamic and electrifying atmosphere that defines urban life.",
+        "photoCover": "https://images.unsplash.com/photo-1690188618606-ab94046e983c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1826&q=80"
+    },
+    {
+        "id":"4",
+        "title":"Exquisite Gourmet Cuisine",
+        "description":"Indulge in a culinary journey with our exquisite gourmet cuisine. From meticulously crafted dishes to mouthwatering desserts, our menu offers a symphony of flavors and presentation that will delight your senses and leave you craving for more.",
+        "photoCover": "https://images.unsplash.com/photo-1611143669185-af224c5e3252?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80"
+    },
 ]
